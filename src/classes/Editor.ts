cb5d904d6acbfde703179a38cb49c3ab65342357
Emanuel <emanuel.palestino.h@gmail.com>
2023-05-19 15:37:19 -0600

    Created editor class

---
+++ src/classes/Editor.ts
@@ -0,0 +1,73 @@
+import { Edge } from './Edge'
+import { Node } from './Node'
+
+export class Editor {
+	private canvas: SVGSVGElement
+	private nodes: Node[]
+	private edges: Edge[]
+
+	constructor(canvas: SVGSVGElement) {
+		this.canvas = canvas
+		this.canvas.setAttribute('viewBox', `0 0 ${this.canvas.clientWidth} ${this.canvas.clientHeight}`)
+		this.nodes = []
+		this.edges = []
+
+		this.clickHandler = this.clickHandler.bind(this)
+		this.mouseDownHandler = this.mouseDownHandler.bind(this)
+		this.mouseMoveHandler = this.mouseMoveHandler.bind(this)
+		this.mouseUpHandler = this.mouseUpHandler.bind(this)
+
+	}
+
+	public init(): void {
+		this.setupEventListener()
+	}
+
+	private setupEventListener(): void {
+		this.canvas.addEventListener('click', this.clickHandler)
+		this.canvas.addEventListener('mousedown', this.mouseDownHandler)
+		this.canvas.addEventListener('mouseup', this.mouseUpHandler)
+		this.canvas.addEventListener('mouseleave', this.mouseUpHandler)
+	}
+
+	private clickHandler(e: MouseEvent): void {
+		let x: number = e.offsetX
+		let y: number = e.offsetY
+		let target = e.target as SVGSVGElement
+
+		// Exit if the click was not on the canvas
+		if (target.id !== 'canvas') return
+
+		// Create a new node
+		let node = new Node(x, y)
+		this.canvas.appendChild(node.node)
+		this.nodes.push(node)
+	}
+
+	private mouseDownHandler(e: MouseEvent): void {
+		let target = e.target as SVGSVGElement
+
+		if (target.classList.contains('node')) {
+			// Set the node as the one being dragged
+			let id = target.id
+			Node.nodeDragged = this.nodes.find(node => node.id === id) || null
+			Node.nodeDragged?.startMove()
+		}
+
+		this.canvas.addEventListener('mousemove', this.mouseMoveHandler)
+	}
+
+	private mouseMoveHandler(e: MouseEvent): void {
+		if (!Node.nodeDragged) return
+		
+		e.preventDefault()
+		Node.nodeDragged.move(e.offsetX, e.offsetY)
+	}
+
+	private mouseUpHandler(e: MouseEvent): void {
+		Node.nodeDragged?.endMove()
+		Node.nodeDragged = null
+		this.canvas.removeEventListener('mousemove', this.mouseMoveHandler)
+	}
+
+}
--- src/editor.ts
+++ src/editor.ts
@@ -1,57 +1,6 @@
-import { Edge } from "./classes/Edge"
-import { Node } from "./classes/Node"
+import { Editor } from "./classes/Editor"
 
 let SVGCanvas: SVGSVGElement = document.querySelector<SVGSVGElement>('#canvas') || document.createElementNS('http://www.w3.org/2000/svg', 'svg')
-let nodes: Node[] = []
-let edges: Edge[] = []
 
-SVGCanvas.setAttribute('viewBox', `0 0 ${SVGCanvas.clientWidth} ${SVGCanvas.clientHeight}`)
-
-SVGCanvas.addEventListener('click', (e: MouseEvent) => {
-	let x: number = e.offsetX
-	let y: number = e.offsetY
-	let target = e.target as SVGSVGElement
-
-	// Exit if the click was not on the canvas
-	if (target.id !== 'canvas') return
-
-	// Create a new node
-	let node = new Node(x, y)
-	SVGCanvas.appendChild(node.node)
-	nodes.push(node)
-})
-
-
-SVGCanvas.addEventListener('mousedown', (e: MouseEvent) => {
-	let target = e.target as SVGSVGElement
-
-	if (target.classList.contains('node')) {
-		// Set the node as the one being dragged
-		let id = target.id
-		Node.nodeDragged = nodes.find(node => node.id === id) || null
-		Node.nodeDragged?.startMove()
-	}
-
-	SVGCanvas.addEventListener('mousemove', mouseMoveHandler)
-})
-
-const mouseMoveHandler = (e: MouseEvent) => {
-	if (Node.nodeDragged) {
-		let x: number = e.offsetX
-		let y: number = e.offsetY
-
-		Node.nodeDragged.move(x, y)
-	}
-}
-
-SVGCanvas.addEventListener('mouseup', () => {
-	Node.nodeDragged?.endMove()
-	Node.nodeDragged = null
-	SVGCanvas.removeEventListener('mousemove', mouseMoveHandler)
-})
-
-SVGCanvas.addEventListener('mouseleave', () => {
-	Node.nodeDragged?.endMove()
-	Node.nodeDragged = null
-	SVGCanvas.removeEventListener('mousemove', mouseMoveHandler)
-})
+const editor = new Editor(SVGCanvas)
+editor.init()
