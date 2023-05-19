a19ece62262b52822fd92208e3600f766423ab48
Emanuel <emanuel.palestino.h@gmail.com>
2023-05-19 15:05:51 -0600

    Implemented create and drag nodes

--- src/classes/Edge.ts
+++ src/classes/Edge.ts
@@ -1,4 +1,4 @@
-import { Node } from "./Node"
+import { Node } from './Node'
 
 export class Edge {
 	private edge: SVGGElement
--- src/classes/Node.ts
+++ src/classes/Node.ts
@@ -1,12 +1,15 @@
 export class Node {
+	// SVG elements
 	public node: SVGGElement
 	private circle: SVGCircleElement
 	private name: SVGTextElement
 
+	// Public node properties
 	public x: number
 	public y: number
 	public id: string
 
+	// Private node properties
 	private state: string
 	private distance: number
 	private finished: boolean
@@ -14,36 +17,62 @@ export class Node {
 	private edges: any[]
 
 	public static nodeCount: number = 0
+	public static nodeDragged: Node | null = null
 
-	constructor(name: string, x: number, y: number) {
+	constructor(x: number, y: number, name: string = '') {
 		Node.nodeCount++
 
 		this.x = x
 		this.y = y
 
+		// Create node container
 		this.node = document.createElementNS('http://www.w3.org/2000/svg', 'g')
-		this.node.setAttribute('class', 'full-node')
+		this.node.setAttributeNS(null, 'class', 'full-node')
 
+		// Create node circle
 		this.circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle')
-		this.circle.setAttribute('class', 'node')
-		this.circle.setAttribute('id', `node_${name}`)
-		this.circle.setAttribute('cx', String(this.x))
-		this.circle.setAttribute('cy', String(this.y))
+		this.circle.setAttributeNS(null, 'class', 'node')
+		this.circle.setAttributeNS(null, 'id', `node_${name ? name : Node.nodeCount}`)
+		this.circle.setAttributeNS(null, 'cx', String(this.x))
+		this.circle.setAttributeNS(null, 'cy', String(this.y))
 
+		// Create node name
 		this.name = document.createElementNS('http://www.w3.org/2000/svg', 'text')
-		this.name.setAttribute('x', String(this.x))
-		this.name.setAttribute('y', String(this.y - 22))
-		this.name.setAttribute('for-node', name)
-		this.name.textContent = name
+		this.name.setAttributeNS(null, 'x', String(this.x))
+		this.name.setAttributeNS(null, 'y', String(this.y - 22))
+		this.name.setAttributeNS(null, 'alignment-baseline', 'after-edge')
+		this.name.setAttributeNS(null, 'for-node', name ? name : String(Node.nodeCount))
+		this.name.textContent = name ? name : String(Node.nodeCount)
 
+		// Append elements to node container
 		this.node.appendChild(this.circle)
 		this.node.appendChild(this.name)
 
-		this.id = `node_${name}`
+		// Set node properties
+		this.id = `node_${name ? name : Node.nodeCount}`
 		this.state = 'unvisited'
 		this.distance = Infinity
 		this.finished = false
 		this.previous = null
 		this.edges = []
+	}
+
+	public startMove(): void {
+		this.circle.classList.add('grabbed')
+	}
+
+	public move(x: number, y: number): void {
+		this.x = x
+		this.y = y
+
+		this.circle.setAttributeNS(null, 'cx', String(this.x))
+		this.circle.setAttributeNS(null, 'cy', String(this.y))
+
+		this.name.setAttributeNS(null, 'x', String(this.x))
+		this.name.setAttributeNS(null, 'y', String(this.y - 22))
+	}
+
+	public endMove(): void {
+		this.circle.classList.remove('grabbed')
 	}
 }
